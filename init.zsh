76144e00265b7e4811392160624fc5e00e5b306b
Joke de Buhr <joke@xckk.de>
2021-10-30 16:17:02 +0200

    restructure

--- init.zsh
+++ init.zsh
@@ -3,6 +3,6 @@ if (( ! ${+commands[kubectl]} )); then
 fi
 
 if [[ ! -e ${0:h}/kubectl.zsh || ${0:h}/kubectl.zsh -ot ${commands[kubectl]} ]]; then
-  kubectl completion zsh >| ${0:h}/kubectl.zsh
+  ${commands[kubectl]} completion zsh >| ${0:h}/kubectl.zsh
 fi
 source ${0:h}/kubectl.zsh
